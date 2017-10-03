f97ba8f52f6e1612c7688e597d2a622110bc0dea
MFrizzy <at.frizzy@gmail.com>
2017-10-03 10:29:22 +0200

    Init

---
+++ unidev.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE PROCEDURE AjouterJourneeTravail (
+  p_codeSalarie TRAVAILLER.codeSalarie%TYPE,
+  p_codeProjet TRAVAILLER.codeProjet%TYPE,
+  p_dateTravail Travailler.DATETRAVAIL%TYPE
+) IS
+
+  BEGIN
+    INSERT INTO TRAVAILLER VALUES ()
+
+  END;
